8df41902b530fc1c8552165cbad3cb5159bacd63
olekskhom <oleks.khom@gmail.com>
2023-10-02 01:17:27 +0300

    add_fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,22 @@ import css from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const localeStorageData = JSON.parse(localStorage.getItem('contacts'));
+    if (localeStorageData) {
+      this.setState({ contacts: localeStorageData });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleInputChange = event => {
     this.setState({ [event.target.name]: event.target.value });
